7d1ea136fe02f5bbe57052a642cc3de5e9adb378
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-09-13 22:29:08 +0530

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -4,3 +4,14 @@ print(22)
 print("10+20=",10+20)
 age=21
 print("Age",age)
+print(type(age))
+
+old=False
+a=None
+print(type(old))
+print(type(a))
+name=input("Enter the name")
+print("Name ",name)
+print(type(name),name)
+num= int(input("Enter the number "))
+print(num)
