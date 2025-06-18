625001c9971119adde8039ffd258d9895354ac00
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-06-18 10:12:03 +0530

    Lec 1

---
+++ python Sessions/fileHandling.py
@@ -0,0 +1,10 @@
+# f=open("D:\Python\python Sessions\demo.txt","a")
+# f.write("you are a gandu")
+# f=open("D:\Python\python Sessions\demo.txt","r")
+# print(f.read())
+
+# f=open("CSK.txt","x")
+# f.close()
+
+import os
+os.remove("D:\Python\python Sessions\CSK.txt")
